2e5409d3da9de0a19df16f2ae13677c9413cd71e
sveltifier <pete.montgomery@ons.gov.uk>
2025-02-06 10:27:43 +0000

    Updates

---
+++ assets/intro.stories-7746162f.js
@@ -0,0 +1,29 @@
+import{M as i}from"./chunk-PCJTTTQV-50d5c029.js";import{p as r,j as e}from"./jsx-runtime-539b76a3.js";import{u as a}from"./index-297ebefe.js";import"./iframe-4cb71cf4.js";import"../sb-preview/runtime.js";import"./extends-b40c5bac.js";import"./_commonjsHelpers-725317a4.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-1a79a776.js";import"./index-356e4a49.js";function o(n){const t=Object.assign({h1:"h1",p:"p",strong:"strong",a:"a",h2:"h2",h3:"h3",ul:"ul",li:"li",pre:"pre",code:"code"},a(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(i,{title:"Intro",parameters:{...r}}),`
+`,e.jsx(t.h1,{id:"onsvisualsvelte-components",children:"@onsvisual/svelte-components"}),`
+`,e.jsxs("p",{children:[e.jsx("a",{href:"https://www.npmjs.com/package/@onsvisual/svelte-components",children:e.jsx("img",{src:"https://badge.fury.io/js/@onsvisual%2Fsvelte-components.svg",alt:"npm version",style:{display:"inline-block",margin:"0 5px 0 0"}})}),e.jsx("a",{href:"https://github.com/onsvisual/svelte-components",children:e.jsx("img",{src:"https://badgen.net/badge/icon/GitHub?icon=github&label",alt:"GitHub",style:{display:"inline-block",margin:"0 5px 0 0"}})})]}),`
+`,e.jsxs(t.p,{children:["A library of ONS Svelte components for projects large and small. Its codebase and docs borrow ",e.jsx(t.strong,{children:"heavily"})," from Reuters' fantastic ",e.jsx(t.a,{href:"https://github.com/reuters-graphics/graphics-components",target:"_blank",rel:"nofollow noopener noreferrer",children:"graphics-components"})," library."]}),`
+`,e.jsxs(t.p,{children:["The components and styles in this library are largely based on the ",e.jsx(t.a,{href:"https://service-manual.ons.gov.uk/design-system/",target:"_blank",rel:"nofollow noopener noreferrer",children:"ONS Design System"}),", but with a number of additions and modifications to make them suitable for a range of data visualisation and data journalism projects."]}),`
+`,e.jsx(t.h2,{id:"installing-the-components",children:"Installing the components"}),`
+`,e.jsx(t.p,{children:"There are two ways to start a new project using these components."}),`
+`,e.jsx(t.h3,{id:"1-use-a-ready-made-template",children:"1. Use a ready-made template"}),`
+`,e.jsx(t.p,{children:"When you start a project from one of the following templates, @onsvisual/svelte-components will already be installed:"}),`
+`,e.jsxs(t.ul,{children:[`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://github.com/ONSvisual/sveltekit-starter",target:"_blank",rel:"nofollow noopener noreferrer",children:"sveltekit-starter"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://github.com/ONSvisual/robo-article",target:"_blank",rel:"nofollow noopener noreferrer",children:"robo-article"})}),`
+`,e.jsx(t.li,{children:e.jsx(t.a,{href:"https://github.com/ONSvisual/robo-embed",target:"_blank",rel:"nofollow noopener noreferrer",children:"robo-embed"})}),`
+`]}),`
+`,e.jsx(t.h3,{id:"2-start-a-new-svelte-kit-project-form-scratch",children:"2. Start a new Svelte Kit project form scratch"}),`
+`,e.jsxs(t.p,{children:["If you want to start from scratch, you can set up a ",e.jsx(t.a,{href:"https://kit.svelte.dev/docs/creating-a-project",target:"_blank",rel:"nofollow noopener noreferrer",children:"new Svelte Kit project"}),", then run the following command."]}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-bash",children:`npm install @onsvisual/svelte-components --save-dev
+`})}),`
+`,e.jsx(t.h2,{id:"using-the-components",children:"Using the components"}),`
+`,e.jsxs(t.p,{children:["Checkout the ",e.jsx(t.a,{href:"?path=/docs/guides-using-these-docs--page",children:"guides"}),", or dive straight into the docs to start using components."]}),`
+`,`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-html",children:`<script>
+  import "@onsvisual/svelte-components/css/main.css";
+  import { Section } from "@onsvisual/svelte-components";
+<\/script>
+
+<Section title="Hello world!">This is my first ONS Svelte component.</Section>
+`})})]})}function l(n={}){const{wrapper:t}=Object.assign({},a(),n.components);return t?e.jsx(t,{...n,children:e.jsx(o,{...n})}):o(n)}const c=()=>{throw new Error("Docs-only story")};c.parameters={docsOnly:!0};const s={title:"Intro",parameters:{...r},tags:["stories-mdx"],includeStories:["__page"]};s.parameters=s.parameters||{};s.parameters.docs={...s.parameters.docs||{},page:l};const w=["__page"];export{w as __namedExportsOrder,c as __page,s as default};
+//# sourceMappingURL=intro.stories-7746162f.js.map
