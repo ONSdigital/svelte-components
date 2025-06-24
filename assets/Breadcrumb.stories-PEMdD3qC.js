36d7679b8fe23f7af869d37b5e791e3e3c31858f
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-06-24 15:56:57 +0100

    Updates

---
+++ assets/Breadcrumb.stories-PEMdD3qC.js
@@ -0,0 +1,16 @@
+import{p as m,j as p,f as c,k as l,e as d,h as f}from"./iframe-7cEVqKzQ.js";import{w as u,c as g,i as _,d as b}from"./create-runtime-stories-CKAPtYBs.js";import{B as h}from"./Breadcrumb-BVD_5ige.js";import"./each-Bg74oAVh.js";import"./attributes-GFejj3_h.js";import"./Container-CwbPS2Av.js";import"./slot-BP_Qo1ZM.js";import"./class-Dzh9Da8g.js";import"./attributes-Co-1QMVx.js";import"./style-CcY6orqc.js";import"./Theme-BhHlwmD8.js";import"./svelte-head-B5hjQbmX.js";import"./html-FUsQury4.js";const v=`Displays a breadcrumb with the current page's parents.
+
+Based on [this ONS Design System component](https://service-manual.ons.gov.uk/design-system/components/breadcrumbs).
+
+<!-- prettier-ignore -->
+\`\`\`html
+<script>
+  import { Breadcrumb } from "@onsvisual/svelte-components";
+<\/script>
+
+<Breadcrumb links="{[
+  {label: 'Home', href: '/'},
+  {label: 'Parent page', href: '/parent'}
+]}" />
+\`\`\`
+`,k={title:"Layout/Breadcrumb",component:h,tags:["autodocs"],argTypes:{},parameters:u(v)},{Story:r}=b();var B=p("<!> <!>",1);function t(n,o){m(o,!1),_();var e=B(),a=c(e);r(a,{name:"Default",args:{links:[{label:"Home",href:"/"},{label:"Parent page",href:"/parent"}]},parameters:{__svelteCsf:{rawCode:"<Breadcrumb {...args} />"}}});var i=l(a,2);r(i,{name:"Single back link",args:{href:"/"},parameters:{__svelteCsf:{rawCode:"<Breadcrumb {...args} />"}}}),d(n,e),f()}t.__docgen={data:[],name:"Breadcrumb.stories.svelte"};const s=g(t,k),M=["Default","SingleBackLink"],N={...s.Default,tags:["svelte-csf-v5"]},R={...s.SingleBackLink,tags:["svelte-csf-v5"]};export{N as Default,R as SingleBackLink,M as __namedExportsOrder,k as default};
