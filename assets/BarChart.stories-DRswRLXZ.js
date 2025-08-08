c7a0f1ec6470efe2312e6b472bcdf49654e47a7e
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-08-08 15:45:48 +0100

    Updates

---
+++ assets/BarChart.stories-DRswRLXZ.js
@@ -0,0 +1,20 @@
+import{p as B,j as S,f as k,g as e,o as r,k as s,e as K,h as x}from"./iframe-1HNJ2TKf.js";import{w as b,c as w,i as D,d as N}from"./create-runtime-stories-indmQy6s.js";import{B as O}from"./BarChart-v_E-3WcS.js";import"./DotPlotChart.svelte_svelte_type_style_lang-B2uKmK1u.js";import{d as a}from"./data-C2fm6GRW.js";import"./slot-BP_Qo1ZM.js";import"./style-CcY6orqc.js";import"./attributes-Co-1QMVx.js";import"./this-BYxpbdLP.js";import"./misc-50KEqbRe.js";import"./each-BrKnNkDj.js";import"./attributes-Dn0uQZds.js";import"./class-Dzh9Da8g.js";import"./Legend-CqO54HUt.js";import"./defaultLocale-DJ2q5QjE.js";import"./linear-CZTuTj73.js";import"./band-_dGXGkJA.js";import"./actions-ZTZf_qzC.js";import"./svelte-head-C7vqD3r2.js";const $=`A bar chart component with various modes, plus animation and events.
+
+Note: This component is part of the **@onsvisual/svelte-charts** library. See [more examples here](https://onsvisual.github.io/svelte-charts/).
+
+<!-- prettier-ignore -->
+\`\`\`html
+<script>
+  import { BarChart } from "@onsvisual/svelte-charts";
+
+  const data = [
+    { year: 2017, value: 320 },
+    { year: 2018, value: 640 },
+    { year: 2019, value: 1600 },
+    { year: 2020, value: 3840 },
+  ];
+<\/script>
+
+<BarChart {data} xKey="value" yKey="year" title="Bar chart example" footer="Source: ONS." />
+\`\`\`
+`,G={title:"Data visualisation/BarChart",component:O,tags:["autodocs"],argTypes:{mode:{options:["default","stacked","grouped","comparison","marker"],control:{type:"select"}}},parameters:b($)},{Story:t}=N();var M=S("<!> <!> <!> <!> <!>",1);function d(l,u){B(u,!1),D();var n=M(),p=k(n);const v=r(()=>({data:a.filter(C=>C.group=="apples"),xKey:"value",yKey:"year",title:"Simple bar chart",footer:"Source: ONS.",mode:"default"}));t(p,{name:"Default",get args(){return e(v)},parameters:{__svelteCsf:{rawCode:"<BarChart {...args} />"}}});var i=s(p,2);const _=r(()=>({data:a,xKey:"value",yKey:"year",zKey:"group",title:"Stacked bar chart",footer:"Source: ONS.",mode:"default"}));t(i,{name:"Stacked bars",get args(){return e(_)},parameters:{__svelteCsf:{rawCode:"<BarChart {...args} />"}}});var m=s(i,2);const f=r(()=>({data:a,xKey:"value",yKey:"year",zKey:"group",title:"Grouped bar chart",footer:"Source: ONS.",mode:"grouped"}));t(m,{name:"Grouped bars",get args(){return e(f)},parameters:{__svelteCsf:{rawCode:"<BarChart {...args} />"}}});var c=s(m,2);const g=r(()=>({data:a,xKey:"value",yKey:"year",zKey:"group",title:"Comparison marker chart",footer:"Source: ONS.",mode:"comparison"}));t(c,{name:"Comparison markers",get args(){return e(g)},parameters:{__svelteCsf:{rawCode:"<BarChart {...args} />"}}});var y=s(c,2);const h=r(()=>({data:a,xKey:"value",yKey:"year",zKey:"group",title:"Barcode chart",footer:"Source: ONS.",mode:"marker"}));t(y,{name:"Barcode",get args(){return e(h)},parameters:{__svelteCsf:{rawCode:"<BarChart {...args} />"}}}),K(l,n),x()}d.__docgen={data:[],name:"BarChart.stories.svelte"};const o=w(d,G),Z=["Default","StackedBars","GroupedBars","ComparisonMarkers","Barcode"],ee={...o.Default,tags:["svelte-csf-v5"]},re={...o.StackedBars,tags:["svelte-csf-v5"]},ae={...o.GroupedBars,tags:["svelte-csf-v5"]},te={...o.ComparisonMarkers,tags:["svelte-csf-v5"]},oe={...o.Barcode,tags:["svelte-csf-v5"]};export{oe as Barcode,te as ComparisonMarkers,ee as Default,ae as GroupedBars,re as StackedBars,Z as __namedExportsOrder,G as default};
---
+++ assets/Footer.stories-CPOdbokS.js
@@ -0,0 +1,11 @@
+import{p as c,j as l,f,k as s,e as _,h as d}from"./iframe-1HNJ2TKf.js";import{w as v,c as g,i as h,d as u}from"./create-runtime-stories-indmQy6s.js";import{F as C}from"./Footer-BbtYc3-P.js";import"./attributes-Dn0uQZds.js";import"./Theme-BOGACQeJ.js";import"./svelte-head-C7vqD3r2.js";import"./html-CfxAF0H0.js";import"./slot-BP_Qo1ZM.js";import"./class-Dzh9Da8g.js";import"./attributes-Co-1QMVx.js";import"./style-CcY6orqc.js";const D=`A footer component with compact mode and theming options.
+
+<!-- prettier-ignore -->
+\`\`\`html
+<script>
+  import { Footer } from "@onsvisual/svelte-components";
+<\/script>
+
+<Footer />
+\`\`\`
+`,F={title:"Layout/Footer",component:C,tags:["autodocs"],argTypes:{theme:{control:{type:"select"}}},parameters:{...v(D),layout:"fullscreen"}},{Story:t}=u();var k=l("<!> <!> <!>",1);function n(m,p){c(p,!1),h();var o=k(),a=f(o);t(a,{name:"Default",args:{},parameters:{__svelteCsf:{rawCode:"<Footer {...args} />"}}});var r=s(a,2);t(r,{name:"Compact variant",args:{compact:!0},parameters:{__svelteCsf:{rawCode:"<Footer {...args} />"}}});var i=s(r,2);t(i,{name:"Dark theme",args:{theme:"dark"},parameters:{__svelteCsf:{rawCode:"<Footer {...args} />"}}}),_(m,o),d()}n.__docgen={data:[],name:"Footer.stories.svelte"};const e=g(n,F),L=["Default","CompactVariant","DarkTheme"],M={...e.Default,tags:["svelte-csf-v5"]},O={...e.CompactVariant,tags:["svelte-csf-v5"]},R={...e.DarkTheme,tags:["svelte-csf-v5"]};export{O as CompactVariant,R as DarkTheme,M as Default,L as __namedExportsOrder,F as default};
