7045d8aee6128410704345f11f2fb7297e304a6c
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-08-08 14:41:14 +0100

    added storybook preview note to sveltekit route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>ONS Svelte components library</h1>
+<p>You can preview the components by running this command in the terminal:</p>
+<p><strong>npm run storybook</strong></p>
