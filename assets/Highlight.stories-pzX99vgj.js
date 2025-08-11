adb0c1a19812c09c9b5f93b794a53e7cd876ae2d
Ahmad Barclay <Ahmad.Barclay@ons.gov.uk>
2025-08-11 11:52:02 +0100

    Updates

---
+++ assets/Highlight.stories-pzX99vgj.js
@@ -0,0 +1,17 @@
+import{p,Z as l,h as m,_ as c,j as h,t as g,o as f,q as u,n as _,e as d}from"./iframe--x4qF4Cz.js";import{w as v,c as H,i as D,d as $}from"./create-runtime-stories-BnfwZkSc.js";import{H as o}from"./Highlight-CGFpKh7c.js";import{l as w}from"./utils-B8WOfuUY.js";import"./slot-BP_Qo1ZM.js";import"./class-Dzh9Da8g.js";import"./attributes-Co-1QMVx.js";import"./style-CcY6orqc.js";import"./Container-Ril63Z3a.js";import"./attributes-DUgS1wvt.js";import"./Theme-BgEiTc1S.js";import"./svelte-head-DACCs42k.js";import"./html-zSkhiqJs.js";import"./_commonjsHelpers-CqkleIqs.js";const x=`A large text block that can be used as a standfirst or transition between sections of a feature article, with various options and style customisations.
+
+<!-- prettier-ignore -->
+\`\`\`html
+<script>
+  import { Highlight } from "@onsvisual/svelte-components";
+<\/script>
+
+<Highlight>
+  <p>Contents of highlight section.</p>
+</Highlight>
+\`\`\`
+`,y=(t,e=l)=>{o(t,c(e,{children:(i,j)=>{var s=q(),r=_(s);g(n=>u(r,n),[()=>w(1)],f),d(i,s)},$$slots:{default:!0}}))},C={title:"Layout/Highlight",component:o,tags:["autodocs"],argTypes:{width:{control:{type:"select"}}},parameters:v(x)},{Story:b}=$();var q=h("<p> </p>");function a(t,e){p(e,!1),D(),b(t,{name:"Default",args:{},get template(){return y},parameters:{__svelteCsf:{rawCode:`<Highlight {...args}>
+	<p>
+		{lipsum(1)}
+	</p>
+</Highlight>`}}}),m()}a.__docgen={data:[],name:"Highlight.stories.svelte"};const S=H(a,C),I=["Default"],J={...S.Default,tags:["svelte-csf-v5"]};export{J as Default,I as __namedExportsOrder,C as default};
