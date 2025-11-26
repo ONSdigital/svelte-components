68c4058641f5a9e8f9a1f4643f807fdd79eb5d83
Barclay <Ahmad.Barclay@ons.gov.uk>
2025-11-26 09:55:42 +0000

    Updates

---
+++ assets/Divider.stories-DAcPepLA.js
@@ -0,0 +1,13 @@
+import{p as c,j as l,f as p,k as t,e as _,h as D}from"./iframe-DblgdR9c.js";import{w as f,c as g,i as h,d as w}from"./create-runtime-stories-VjmG1RUC.js";import{D as u}from"./Divider-DpyKtIyC.js";import"./class-Dzh9Da8g.js";import"./attributes-Co-1QMVx.js";import"./Container-Denbr5vS.js";import"./slot-BP_Qo1ZM.js";import"./attributes-CRQ3k2v3.js";import"./style-CcY6orqc.js";import"./Theme-DeBAaVhe.js";import"./svelte-head-CTEoEUwJ.js";import"./html-CORolPqq.js";const b=`A component to draw a divider between blocks of contnet.\r
+\r
+<!-- prettier-ignore -->\r
+\`\`\`html\r
+<script>\r
+  import { Divider } from "@onsvisual/svelte-components";\r
+<\/script>\r
+\r
+<!-- Some content blocks -->\r
+<Divider />\r
+<!-- More content blocks -->\r
+\`\`\`\r
+`,C={title:"Decorators/Divider",component:u,tags:["autodocs"],argTypes:{hr:{control:{type:"select"}},width:{control:{type:"select"}}},parameters:f(b)},{Story:e}=w();var M=l("<!> <!> <!> <!>",1);function n(d,v){c(v,!1),h();var i=M(),s=p(i);e(s,{name:"Default",args:{},parameters:{__svelteCsf:{rawCode:"<Divider {...args} />"}}});var o=t(s,2);e(o,{name:"Light mode",args:{mode:"light"},parameters:{__svelteCsf:{rawCode:"<Divider {...args} />"}}});var a=t(o,2);e(a,{name:"Narrow divider",args:{hr:"narrow"},parameters:{__svelteCsf:{rawCode:"<Divider {...args} />"}}});var m=t(a,2);e(m,{name:"Invisible divider",args:{hr:null},parameters:{__svelteCsf:{rawCode:"<Divider {...args} />"}}}),_(d,i),D()}n.__docgen={data:[],name:"Divider.stories.svelte"};const r=g(n,C),R=["Default","LightMode","NarrowDivider","InvisibleDivider"],T={...r.Default,tags:["svelte-csf-v5"]},q={...r.LightMode,tags:["svelte-csf-v5"]},z={...r.NarrowDivider,tags:["svelte-csf-v5"]},B={...r.InvisibleDivider,tags:["svelte-csf-v5"]};export{T as Default,B as InvisibleDivider,q as LightMode,z as NarrowDivider,R as __namedExportsOrder,C as default};
--- assets/misc-Bq5MG93T.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./iframe-DI485cCl.js";function c(l,r){var e;var a=(e=l.$$events)==null?void 0:e[r.type],s=o(a)?a.slice():a==null?[]:[a];for(var t of s)t.call(this,r)}export{c as b};
