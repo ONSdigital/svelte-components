278865985f4c285ca2d96b8611510e05bacf9c12
Barclay <Ahmad.Barclay@ons.gov.uk>
2026-01-05 12:04:51 +0000

    Updates

---
+++ assets/Breadcrumb.stories--Q7HBbki.js
@@ -0,0 +1,16 @@
+import{p as m,j as p,f as c,k as l,e as d,h as f}from"./iframe-DNxkqxAS.js";import{w as u,c as g,i as _,d as b}from"./create-runtime-stories-CizDzvZP.js";import{B as h}from"./Breadcrumb-V4RBSpWU.js";import"./each-CimI5bI2.js";import"./attributes-BOgqZKN7.js";import"./Container-Ba8YSacQ.js";import"./slot-BP_Qo1ZM.js";import"./class-Dzh9Da8g.js";import"./attributes-Co-1QMVx.js";import"./style-CcY6orqc.js";import"./Theme-weBval_s.js";import"./svelte-head-BpEIKJQj.js";import"./html-DJmUfnMu.js";const v=`Displays a breadcrumb with the current page's parents.\r
+\r
+Based on [this ONS Design System component](https://service-manual.ons.gov.uk/design-system/components/breadcrumbs).\r
+\r
+<!-- prettier-ignore -->\r
+\`\`\`html\r
+<script>\r
+  import { Breadcrumb } from "@onsvisual/svelte-components";\r
+<\/script>\r
+\r
+<Breadcrumb links="{[\r
+  {label: 'Home', href: '/'},\r
+  {label: 'Parent page', href: '/parent'}\r
+]}" />\r
+\`\`\`\r
+`,k={title:"Layout/Breadcrumb",component:h,tags:["autodocs"],argTypes:{},parameters:u(v)},{Story:a}=b();var B=p("<!> <!>",1);function t(n,o){m(o,!1),_();var e=B(),r=c(e);a(r,{name:"Default",args:{links:[{label:"Home",href:"/"},{label:"Parent page",href:"/parent"}]},parameters:{__svelteCsf:{rawCode:"<Breadcrumb {...args} />"}}});var i=l(r,2);a(i,{name:"Single back link",args:{href:"/"},parameters:{__svelteCsf:{rawCode:"<Breadcrumb {...args} />"}}}),d(n,e),f()}t.__docgen={data:[],name:"Breadcrumb.stories.svelte"};const s=g(t,k),M=["Default","SingleBackLink"],N={...s.Default,tags:["svelte-csf-v5"]},R={...s.SingleBackLink,tags:["svelte-csf-v5"]};export{N as Default,R as SingleBackLink,M as __namedExportsOrder,k as default};
---
+++ assets/svelte-head-BpEIKJQj.js
@@ -0,0 +1,1 @@
+import{F as t,G as o,H as r}from"./iframe-DNxkqxAS.js";function c(e){var a;a=document.head.appendChild(t());try{o(()=>e(a),r)}finally{}}export{c as h};
