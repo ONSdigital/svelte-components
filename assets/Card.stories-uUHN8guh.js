d0e2f2069bdbe9463d2a56427e118f0883cd77ad
Barclay <Ahmad.Barclay@ons.gov.uk>
2026-01-12 09:39:25 +0000

    Updates

---
+++ assets/Card.stories-uUHN8guh.js
@@ -0,0 +1,31 @@
+import{p as L,j as W,f as F,Z as x,k as o,e as d,h as b,_ as g,V as f,t as h,o as v,q as $}from"./iframe-DgZwGItH.js";import{w as y,c as B,i as I,d as P}from"./create-runtime-stories-xZ2YYh46.js";import{l as w}from"./utils-VWytQRnm.js";import{C}from"./Card-BmHRJ4tj.js";import{G as q}from"./Grid-XXhF194j.js";import"./_commonjsHelpers-CqkleIqs.js";import"./slot-BP_Qo1ZM.js";import"./attributes-2UgTCuWr.js";import"./class-Dzh9Da8g.js";import"./attributes-Co-1QMVx.js";import"./GridCell-vfBf3cQs.js";import"./style-CcY6orqc.js";import"./Container-DUXLb5xi.js";import"./Theme-D40FOd9N.js";import"./svelte-head-W05OBU9T.js";import"./html-v461-nDV.js";const S=`Display content and links as cards. Can be used in combination with the \`<Grid>\` component to combine multiple cards.\r
+\r
+<!-- prettier-ignore -->\r
+\`\`\`html\r
+<script>\r
+  import { Grid, Card } from "@onsvisual/svelte-components";\r
+<\/script>\r
+\r
+<Grid>\r
+  <Card title="Card one">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Card>\r
+  <Card title="Card two">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Card>\r
+  <Card title="Card three">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Card>\r
+</Grid>\r
+\`\`\`\r
+`,m=(c,t=x)=>{q(c,{children:(n,_)=>{var e=E(),a=F(e);C(a,g({title:"Card one"},t,{children:(s,G)=>{var r=f();h(i=>$(r,i),[()=>w(1)],v),d(s,r)},$$slots:{default:!0}}));var l=o(a,2);C(l,g({title:"Card two"},t,{children:(s,G)=>{var r=f();h(i=>$(r,i),[()=>w(1,1)],v),d(s,r)},$$slots:{default:!0}}));var D=o(l,2);C(D,g({title:"Card three"},t,{children:(s,G)=>{var r=f();h(i=>$(r,i),[()=>w(1,2)],v),d(s,r)},$$slots:{default:!0}})),d(n,e)},$$slots:{default:!0}})},j={title:"Layout/Card",component:C,tags:["autodocs"],argTypes:{},parameters:y(S)},{Story:p}=P();var E=W("<!> <!> <!>",1),M=W("<!> <!> <!> <!>",1);function k(c,t){L(t,!1),I();var n=M(),_=F(n);p(_,{name:"Default",args:{},get template(){return m},parameters:{__svelteCsf:{rawCode:`<Grid>\r
+	<Card title="Card one" {...args}>{lipsum(1)}</Card>\r
+	<Card title="Card two" {...args}>{lipsum(1, 1)}</Card>\r
+	<Card title="Card three" {...args}>{lipsum(1, 2)}</Card>\r
+</Grid>`}}});var e=o(_,2);p(e,{name:"Cards with links",args:{href:"#0"},get template(){return m},parameters:{__svelteCsf:{rawCode:`<Grid>\r
+	<Card title="Card one" {...args}>{lipsum(1)}</Card>\r
+	<Card title="Card two" {...args}>{lipsum(1, 1)}</Card>\r
+	<Card title="Card three" {...args}>{lipsum(1, 2)}</Card>\r
+</Grid>`}}});var a=o(e,2);p(a,{name:"Featured cards with baseline",args:{mode:"featured",baseline:!0},get template(){return m},parameters:{__svelteCsf:{rawCode:`<Grid>\r
+	<Card title="Card one" {...args}>{lipsum(1)}</Card>\r
+	<Card title="Card two" {...args}>{lipsum(1, 1)}</Card>\r
+	<Card title="Card three" {...args}>{lipsum(1, 2)}</Card>\r
+</Grid>`}}});var l=o(a,2);p(l,{name:"Featured cards with image",args:{mode:"featured",image:"https://service-manual.ons.gov.uk/img/small/placeholder-card.png"},get template(){return m},parameters:{__svelteCsf:{rawCode:`<Grid>\r
+	<Card title="Card one" {...args}>{lipsum(1)}</Card>\r
+	<Card title="Card two" {...args}>{lipsum(1, 1)}</Card>\r
+	<Card title="Card three" {...args}>{lipsum(1, 2)}</Card>\r
+</Grid>`}}}),d(c,n),b()}k.__docgen={data:[],name:"Card.stories.svelte"};const u=B(k,j),tr=["Default","CardsWithLinks","FeaturedCardsWithBaseline","FeaturedCardsWithImage"],er={...u.Default,tags:["svelte-csf-v5"]},ar={...u.CardsWithLinks,tags:["svelte-csf-v5"]},sr={...u.FeaturedCardsWithBaseline,tags:["svelte-csf-v5"]},ir={...u.FeaturedCardsWithImage,tags:["svelte-csf-v5"]};export{ar as CardsWithLinks,er as Default,sr as FeaturedCardsWithBaseline,ir as FeaturedCardsWithImage,tr as __namedExportsOrder,j as default};
---
+++ assets/ColumnChart.stories-zzU4sSx5.js
@@ -0,0 +1,20 @@
+import{p as S,j as k,f as K,g as e,o as r,k as s,e as x,h as w}from"./iframe-DgZwGItH.js";import{w as D,c as B,i as N,d as O}from"./create-runtime-stories-xZ2YYh46.js";import"./DotPlotChart.svelte_svelte_type_style_lang-CkLbrd_R.js";import{C as $}from"./ColumnChart-BFKTvvcI.js";import{d as a}from"./data-C2fm6GRW.js";import"./linear-CZTuTj73.js";import"./slot-BP_Qo1ZM.js";import"./style-CcY6orqc.js";import"./attributes-Co-1QMVx.js";import"./this-C-X3B6bZ.js";import"./attributes-2UgTCuWr.js";import"./each-Caa3eqBD.js";import"./class-Dzh9Da8g.js";import"./actions-CmyQtFvL.js";import"./svelte-head-W05OBU9T.js";import"./misc-DPV9Q2s1.js";import"./Legend-CxFiJtEh.js";import"./band-CZcPyXJ_.js";const b=`A column chart component with various modes, plus animation and events.\r
+\r
+Note: This component is part of the **@onsvisual/svelte-charts** library. See [more examples here](https://onsvisual.github.io/svelte-charts/).\r
+\r
+<!-- prettier-ignore -->\r
+\`\`\`html\r
+<script>\r
+  import { ColumnChart } from "@onsvisual/svelte-charts";\r
+\r
+  const data = [\r
+    { year: 2017, value: 320 },\r
+    { year: 2018, value: 640 },\r
+    { year: 2019, value: 1600 },\r
+    { year: 2020, value: 3840 },\r
+  ];\r
+<\/script>\r
+\r
+<ColumnChart {data} xKey="value" yKey="year" title="Bar chart example" footer="Source: ONS." />\r
+\`\`\`\r
+`,G={title:"Data visualisation/ColumnChart",component:$,tags:["autodocs"],argTypes:{mode:{options:["default","stacked","grouped","comparison","marker"],control:{type:"select"}}},parameters:D(b)},{Story:o}=O();var M=k("<!> <!> <!> <!> <!>",1);function u(c,d){S(d,!1),N();var n=M(),m=K(n);const C=r(()=>({data:a.filter(h=>h.group=="apples"),xKey:"year",yKey:"value",title:"Simple bar chart",footer:"Source: ONS.",mode:"default"}));o(m,{name:"Default",get args(){return e(C)},parameters:{__svelteCsf:{rawCode:"<ColumnChart {...args} />"}}});var l=s(m,2);const v=r(()=>({data:a,xKey:"year",yKey:"value",zKey:"group",title:"Stacked bar chart",footer:"Source: ONS.",mode:"default"}));o(l,{name:"Stacked columns",get args(){return e(v)},parameters:{__svelteCsf:{rawCode:"<ColumnChart {...args} />"}}});var p=s(l,2);const _=r(()=>({data:a,xKey:"year",yKey:"value",zKey:"group",title:"Grouped bar chart",footer:"Source: ONS.",mode:"grouped"}));o(p,{name:"Grouped columns",get args(){return e(_)},parameters:{__svelteCsf:{rawCode:"<ColumnChart {...args} />"}}});var i=s(p,2);const f=r(()=>({data:a,xKey:"year",yKey:"value",zKey:"group",title:"Comparison marker chart",footer:"Source: ONS.",mode:"comparison"}));o(i,{name:"Comparison markers",get args(){return e(f)},parameters:{__svelteCsf:{rawCode:"<ColumnChart {...args} />"}}});var g=s(i,2);const y=r(()=>({data:a,xKey:"year",yKey:"value",zKey:"group",title:"Barcode chart",footer:"Source: ONS.",mode:"marker"}));o(g,{name:"Barcode",get args(){return e(y)},parameters:{__svelteCsf:{rawCode:"<ColumnChart {...args} />"}}}),x(c,n),w()}u.__docgen={data:[],name:"ColumnChart.stories.svelte"};const t=B(u,G),Y=["Default","StackedColumns","GroupedColumns","ComparisonMarkers","Barcode"],Z={...t.Default,tags:["svelte-csf-v5"]},ee={...t.StackedColumns,tags:["svelte-csf-v5"]},re={...t.GroupedColumns,tags:["svelte-csf-v5"]},ae={...t.ComparisonMarkers,tags:["svelte-csf-v5"]},oe={...t.Barcode,tags:["svelte-csf-v5"]};export{oe as Barcode,ae as ComparisonMarkers,Z as Default,re as GroupedColumns,ee as StackedColumns,Y as __namedExportsOrder,G as default};
