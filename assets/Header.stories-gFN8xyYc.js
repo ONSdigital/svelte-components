ea2b2724634913da7b5076364539ca2d0a248a38
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2026-04-15 11:16:58 +0100

    Updates

---
+++ assets/Header.stories-gFN8xyYc.js
@@ -0,0 +1,12 @@
+import{p as l,j as d,f as _,k as a,e as g,h as f}from"./iframe-DM5jLkZ5.js";import{w as C,c as h,i as v,d as u}from"./create-runtime-stories-7hV928Rm.js";import{H as D}from"./Header--nWAISDw.js";import"./slot-BP_Qo1ZM.js";import"./attributes-CR66kYIi.js";import"./Theme-BUQ0zn27.js";import"./svelte-head-f19DVCNd.js";import"./html-B5S8Hroj.js";import"./class-Dzh9Da8g.js";import"./attributes-Co-1QMVx.js";import"./style-CcY6orqc.js";import"./SkipLink-lvNQPgMx.js";import"./each-xZ6zTurB.js";const H=`A header component with compact mode and theming options.
+
+<!-- prettier-ignore -->
+\`\`\`html
+<script>
+  import { Header } from "@onsvisual/svelte-components";
+<\/script>
+
+<Header />
+<!-- page body -->
+\`\`\`
+`,T={title:"Layout/Header",component:D,tags:["autodocs"],argTypes:{theme:{control:{type:"select"}}},parameters:{...C(H),layout:"fullscreen"}},{Story:e}=u();var w=d("<!> <!> <!> <!>",1);function p(n,c){l(c,!1),v();var o=w(),r=_(o);e(r,{name:"Default",args:{},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}});var s=a(r,2);e(s,{name:"Compact",args:{compact:!0},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}});var m=a(s,2);e(m,{name:"Compact with page title",args:{compact:!0,title:"Page title"},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}});var i=a(m,2);e(i,{name:"Compact dark theme",args:{compact:!0,title:"Page title",theme:"dark"},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}}),g(n,o),f()}p.__docgen={data:[],name:"Header.stories.svelte"};const t=h(p,T),O=["Default","Compact","CompactWithPageTitle","CompactDarkTheme"],R={...t.Default,tags:["svelte-csf-v5"]},q={...t.Compact,tags:["svelte-csf-v5"]},z={...t.CompactWithPageTitle,tags:["svelte-csf-v5"]},B={...t.CompactDarkTheme,tags:["svelte-csf-v5"]};export{q as Compact,B as CompactDarkTheme,z as CompactWithPageTitle,R as Default,O as __namedExportsOrder,T as default};
