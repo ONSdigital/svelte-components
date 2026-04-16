e6829a44b1a3c41d536c50d24a55d9478be4a305
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2026-04-16 15:33:28 +0100

    Fix new CSS bug in Em component

--- src/lib/components/Em/Em.svelte
+++ src/lib/components/Em/Em.svelte
@@ -34,7 +34,7 @@
 	class:ons-svelte-mark={mode === "default"}
 	class:ons-svelte-badge={mode === "badge"}
 	class:ons-u-nowrap={nowrap}
-	style:background-color={color}
+	style:background={color}
 	style:color={text}
 	style:font-size={fontSize}
 	role="presentation"
