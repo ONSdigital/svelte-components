675b4e7077a124f2523cfe2d9ac5e854fa5324ae
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2026-04-16 16:51:30 +0100

    Updates

---
+++ assets/Divider.stories-DB2_nnfx.js
@@ -0,0 +1,13 @@
+import{A as l,f as c,m as p,J as t,j as _,I as f}from"./iframe-G_izM0Zb.js";import{w as D,c as g,i as h,d as w}from"./create-runtime-stories-CoDuciYS.js";import{D as u}from"./Divider-Bk4nIK4B.js";import"./class-B9SFUanI.js";import"./attributes-q36Eg1F8.js";import"./Container-D1CoYQNr.js";import"./slot-BP_Qo1ZM.js";import"./attributes-PUz8a1ft.js";import"./style-CNURdi_Z.js";import"./Theme-C0kTR4vk.js";import"./svelte-head-D8HCf5ey.js";import"./html-XEpXRX4b.js";const b=`A component to draw a divider between blocks of contnet.
+
+<!-- prettier-ignore -->
+\`\`\`html
+<script>
+  import { Divider } from "@onsvisual/svelte-components";
+<\/script>
+
+<!-- Some content blocks -->
+<Divider />
+<!-- More content blocks -->
+\`\`\`
+`,C={title:"Layout/Divider",component:u,tags:["autodocs"],argTypes:{hr:{control:{type:"select"}},width:{control:{type:"select"}}},parameters:D(b)},{Story:e}=w();var I=c("<!> <!> <!> <!>",1);function n(d,v){l(v,!1),h();var i=I(),s=p(i);e(s,{name:"Default",args:{},parameters:{__svelteCsf:{rawCode:"<Divider {...args} />"}}});var o=t(s,2);e(o,{name:"Light mode",args:{mode:"light"},parameters:{__svelteCsf:{rawCode:"<Divider {...args} />"}}});var a=t(o,2);e(a,{name:"Narrow divider",args:{hr:"narrow"},parameters:{__svelteCsf:{rawCode:"<Divider {...args} />"}}});var m=t(a,2);e(m,{name:"Invisible divider",args:{hr:null},parameters:{__svelteCsf:{rawCode:"<Divider {...args} />"}}}),_(d,i),f()}n.__docgen={data:[],name:"Divider.stories.svelte"};const r=g(n,C),O=["Default","LightMode","NarrowDivider","InvisibleDivider"],R={...r.Default,tags:["svelte-csf-v5"]},T={...r.LightMode,tags:["svelte-csf-v5"]},q={...r.NarrowDivider,tags:["svelte-csf-v5"]},z={...r.InvisibleDivider,tags:["svelte-csf-v5"]};export{R as Default,z as InvisibleDivider,T as LightMode,q as NarrowDivider,O as __namedExportsOrder,C as default};
