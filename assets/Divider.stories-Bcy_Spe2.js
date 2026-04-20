45b7a92042511a08f9d8a4b116683fd4828a12da
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2026-04-20 12:27:33 +0100

    Updates

---
+++ assets/Divider.stories-Bcy_Spe2.js
@@ -0,0 +1,13 @@
+import{A as l,f as c,m as p,J as t,j as _,I as f}from"./iframe-1IuJh7L4.js";import{w as D,c as g,i as h,d as w}from"./create-runtime-stories-Apq51qS_.js";import{D as u}from"./Divider-DBfnPqjM.js";import"./class-B9SFUanI.js";import"./attributes-q36Eg1F8.js";import"./Container-DoyRaJTM.js";import"./slot-BP_Qo1ZM.js";import"./attributes-CgU0H4sF.js";import"./style-CNURdi_Z.js";import"./Theme-z5H0ed_2.js";import"./svelte-head-D30QzNYP.js";import"./html-uKYSvKeB.js";const b=`A component to draw a divider between blocks of contnet.
+
+<!-- prettier-ignore -->
+\`\`\`html
+<script>
+  import { Divider } from "@onsvisual/svelte-components";
+<\/script>
+
+<!-- Some content blocks -->
+<Divider />
+<!-- More content blocks -->
+\`\`\`
+`,C={title:"Layout/Divider",component:u,tags:["autodocs"],argTypes:{hr:{control:{type:"select"}},width:{control:{type:"select"}}},parameters:D(b)},{Story:e}=w();var I=c("<!> <!> <!> <!>",1);function n(d,v){l(v,!1),h();var i=I(),s=p(i);e(s,{name:"Default",args:{},parameters:{__svelteCsf:{rawCode:"<Divider {...args} />"}}});var o=t(s,2);e(o,{name:"Light mode",args:{mode:"light"},parameters:{__svelteCsf:{rawCode:"<Divider {...args} />"}}});var a=t(o,2);e(a,{name:"Narrow divider",args:{hr:"narrow"},parameters:{__svelteCsf:{rawCode:"<Divider {...args} />"}}});var m=t(a,2);e(m,{name:"Invisible divider",args:{hr:null},parameters:{__svelteCsf:{rawCode:"<Divider {...args} />"}}}),_(d,i),f()}n.__docgen={data:[],name:"Divider.stories.svelte"};const r=g(n,C),O=["Default","LightMode","NarrowDivider","InvisibleDivider"],R={...r.Default,tags:["svelte-csf-v5"]},T={...r.LightMode,tags:["svelte-csf-v5"]},q={...r.NarrowDivider,tags:["svelte-csf-v5"]},z={...r.InvisibleDivider,tags:["svelte-csf-v5"]};export{R as Default,z as InvisibleDivider,T as LightMode,q as NarrowDivider,O as __namedExportsOrder,C as default};
---
+++ assets/svelte-head-D30QzNYP.js
@@ -0,0 +1,1 @@
+import{n as o,o as t,H as n}from"./iframe-1IuJh7L4.js";function c(e){var a;a=document.head.appendChild(o());try{t(()=>e(a),n)}finally{}}export{c as h};
