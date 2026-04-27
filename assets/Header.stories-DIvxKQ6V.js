066c4c996e2601d1587f01dbf1f6848e70cfff89
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2026-04-27 16:45:10 +0100

    Updates

---
+++ assets/Header.stories-DIvxKQ6V.js
@@ -0,0 +1,12 @@
+import{A as _,f as g,m as f,J as t,j as v,I as C}from"./iframe-rn-CYE4_.js";import{w as h,c as u,i as H,d as D}from"./create-runtime-stories-DUShD_-p.js";import{H as k}from"./Header-DAo13b7D.js";import"./slot-BP_Qo1ZM.js";import"./attributes-DzjqHavG.js";import"./Theme-BNgrOjA5.js";import"./svelte-head-FdOH6T_E.js";import"./html-DutQzZpu.js";import"./class-B9SFUanI.js";import"./attributes-q36Eg1F8.js";import"./style-CNURdi_Z.js";import"./SkipLink-D0ATZVSy.js";import"./each-CTmPshpe.js";import"./this-Du894LZY.js";const y=`A header component with compact mode and theming options.
+
+<!-- prettier-ignore -->
+\`\`\`html
+<script>
+  import { Header } from "@onsvisual/svelte-components";
+<\/script>
+
+<Header />
+<!-- page body -->
+\`\`\`
+`,w={title:"Layout/Header",component:k,tags:["autodocs"],argTypes:{theme:{control:{type:"select"}}},parameters:{...h(y),layout:"fullscreen"}},{Story:e}=D();var T=g("<!> <!> <!> <!> <!> <!>",1);function p(d,i){_(i,!1),H();var r=T(),s=f(r);e(s,{name:"Default",args:{},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}});var o=t(s,2);e(o,{name:"Dark mode",args:{theme:"dark"},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}});var m=t(o,2);e(m,{name:"Compact",args:{compact:!0,headerBorder:!0},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}});var c=t(m,2);e(c,{name:"Compact with page title",args:{compact:!0,title:"Page title"},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}});var n=t(c,2);e(n,{name:"Compact dark theme",args:{compact:!0,title:"Page title",theme:"dark"},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}});var l=t(n,2);e(l,{name:"Legacy header",args:{legacy:!0},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}}),v(d,r),C()}p.__docgen={data:[],name:"Header.stories.svelte"};const a=u(p,w),O=["Default","DarkMode","Compact","CompactWithPageTitle","CompactDarkTheme","LegacyHeader"],R={...a.Default,tags:["svelte-csf-v5"]},q={...a.DarkMode,tags:["svelte-csf-v5"]},z={...a.Compact,tags:["svelte-csf-v5"]},F={...a.CompactWithPageTitle,tags:["svelte-csf-v5"]},G={...a.CompactDarkTheme,tags:["svelte-csf-v5"]},K={...a.LegacyHeader,tags:["svelte-csf-v5"]};export{z as Compact,G as CompactDarkTheme,F as CompactWithPageTitle,q as DarkMode,R as Default,K as LegacyHeader,O as __namedExportsOrder,w as default};
---
+++ assets/Section.stories-Ddgblhb5.js
@@ -0,0 +1,25 @@
+import{A as f,f as l,m as v,V as S,J as m,j as c,I as g,W as h,t as w,h as $,K as C,k as D}from"./iframe-rn-CYE4_.js";import{w as M,c as W,i as x,d as y}from"./create-runtime-stories-DUShD_-p.js";import{S as d}from"./Section-oK4kuvEI.js";import{l as b}from"./utils-VWytQRnm.js";import"./slot-BP_Qo1ZM.js";import"./attributes-DzjqHavG.js";import"./class-B9SFUanI.js";import"./attributes-q36Eg1F8.js";import"./Container-CdUCwqLS.js";import"./style-CNURdi_Z.js";import"./Theme-BNgrOjA5.js";import"./svelte-head-FdOH6T_E.js";import"./html-DutQzZpu.js";import"./_commonjsHelpers-CqkleIqs.js";const k=`A layout block for text content, including an optional title.
+
+<!-- prettier-ignore -->
+\`\`\`html
+<script>
+  import { Section } from "@onsvisual/svelte-components";
+<\/script>
+
+<Section width="{narrow|medium|wide|full}" title="Section title">
+  <p>Contents of section.</p>
+</Section>
+\`\`\`
+`,a=(o,s=S)=>{d(o,h(s,{children:(e,n)=>{var t=j(),i=D(t);w(_=>C(i,_),[b],$),c(e,t)},$$slots:{default:!0}}))},A={title:"Layout/Section",component:d,tags:["autodocs"],argTypes:{width:{control:{type:"select"}}},parameters:M(k)},{Story:r}=y();var j=l("<p> </p>"),q=l("<!> <!> <!>",1);function u(o,s){f(s,!1),x();var e=q(),n=v(e);r(n,{name:"Default",args:{title:"Section title"},get template(){return a},parameters:{__svelteCsf:{rawCode:`<Section {...args}>
+	<p>
+		{lipsum()}
+	</p>
+</Section>`}}});var t=m(n,2);r(t,{name:"Medium",args:{width:"medium",title:"Section title"},get template(){return a},parameters:{__svelteCsf:{rawCode:`<Section {...args}>
+	<p>
+		{lipsum()}
+	</p>
+</Section>`}}});var i=m(t,2);r(i,{name:"Wide",args:{width:"wide",title:"Section title"},get template(){return a},parameters:{__svelteCsf:{rawCode:`<Section {...args}>
+	<p>
+		{lipsum()}
+	</p>
+</Section>`}}}),c(o,e),g()}u.__docgen={data:[],name:"Section.stories.svelte"};const p=W(u,A),H=["Default","Medium","Wide"],N={...p.Default,tags:["svelte-csf-v5"]},Q={...p.Medium,tags:["svelte-csf-v5"]},U={...p.Wide,tags:["svelte-csf-v5"]};export{N as Default,Q as Medium,U as Wide,H as __namedExportsOrder,A as default};
