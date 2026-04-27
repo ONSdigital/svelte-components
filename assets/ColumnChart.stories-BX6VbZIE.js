6b6110c656277559169e3082b3f8d18ebf5cde70
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2026-04-27 09:55:16 +0100

    Updates

---
+++ assets/ColumnChart.stories-BX6VbZIE.js
@@ -0,0 +1,20 @@
+import{A as S,f as K,m as k,G as e,h as r,J as s,j as x,I as w}from"./iframe-CfOETbja.js";import{w as B,c as D,i as G,d as N}from"./create-runtime-stories-COP3GmsY.js";import"./DotPlotChart.svelte_svelte_type_style_lang-CknpRzRA.js";import{C as O}from"./ColumnChart-D7iQ96sR.js";import{d as a}from"./data-C2fm6GRW.js";import"./linear-CZTuTj73.js";import"./slot-BP_Qo1ZM.js";import"./style-CNURdi_Z.js";import"./attributes-q36Eg1F8.js";import"./this-BVQbHggA.js";import"./attributes-SGbATqK1.js";import"./each-y_omv-tB.js";import"./class-B9SFUanI.js";import"./actions-BF0BegAT.js";import"./svelte-head-BSK0fUvh.js";import"./misc-BXGqUDGa.js";import"./Legend-XV2nmEis.js";import"./band-BlmrWQck.js";const $=`A column chart component with various modes, plus animation and events.
+
+Note: This component is part of the **@onsvisual/svelte-charts** library. See [more examples here](https://onsvisual.github.io/svelte-charts/).
+
+<!-- prettier-ignore -->
+\`\`\`html
+<script>
+  import { ColumnChart } from "@onsvisual/svelte-charts";
+
+  const data = [
+    { year: 2017, value: 320 },
+    { year: 2018, value: 640 },
+    { year: 2019, value: 1600 },
+    { year: 2020, value: 3840 },
+  ];
+<\/script>
+
+<ColumnChart {data} xKey="value" yKey="year" title="Bar chart example" footer="Source: ONS." />
+\`\`\`
+`,b={title:"Charts/ColumnChart",component:O,tags:["autodocs"],argTypes:{mode:{options:["default","stacked","grouped","comparison","marker"],control:{type:"select"}}},parameters:B($)},{Story:o}=N();var M=K("<!> <!> <!> <!> <!>",1);function i(c,d){S(d,!1),G();var n=M(),m=k(n);const C=r(()=>({data:a.filter(h=>h.group=="apples"),xKey:"year",yKey:"value",title:"Simple bar chart",footer:"Source: ONS.",mode:"default"}));o(m,{name:"Default",get args(){return e(C)},parameters:{__svelteCsf:{rawCode:"<ColumnChart {...args} />"}}});var l=s(m,2);const v=r(()=>({data:a,xKey:"year",yKey:"value",zKey:"group",title:"Stacked bar chart",footer:"Source: ONS.",mode:"default"}));o(l,{name:"Stacked columns",get args(){return e(v)},parameters:{__svelteCsf:{rawCode:"<ColumnChart {...args} />"}}});var p=s(l,2);const _=r(()=>({data:a,xKey:"year",yKey:"value",zKey:"group",title:"Grouped bar chart",footer:"Source: ONS.",mode:"grouped"}));o(p,{name:"Grouped columns",get args(){return e(_)},parameters:{__svelteCsf:{rawCode:"<ColumnChart {...args} />"}}});var u=s(p,2);const f=r(()=>({data:a,xKey:"year",yKey:"value",zKey:"group",title:"Comparison marker chart",footer:"Source: ONS.",mode:"comparison"}));o(u,{name:"Comparison markers",get args(){return e(f)},parameters:{__svelteCsf:{rawCode:"<ColumnChart {...args} />"}}});var y=s(u,2);const g=r(()=>({data:a,xKey:"year",yKey:"value",zKey:"group",title:"Barcode chart",footer:"Source: ONS.",mode:"marker"}));o(y,{name:"Barcode",get args(){return e(g)},parameters:{__svelteCsf:{rawCode:"<ColumnChart {...args} />"}}}),x(c,n),w()}i.__docgen={data:[],name:"ColumnChart.stories.svelte"};const t=D(i,b),Y=["Default","StackedColumns","GroupedColumns","ComparisonMarkers","Barcode"],Z={...t.Default,tags:["svelte-csf-v5"]},ee={...t.StackedColumns,tags:["svelte-csf-v5"]},re={...t.GroupedColumns,tags:["svelte-csf-v5"]},ae={...t.ComparisonMarkers,tags:["svelte-csf-v5"]},oe={...t.Barcode,tags:["svelte-csf-v5"]};export{oe as Barcode,ae as ComparisonMarkers,Z as Default,re as GroupedColumns,ee as StackedColumns,Y as __namedExportsOrder,b as default};
---
+++ assets/EmbedArticle.stories-D0SDZWd7.js
@@ -0,0 +1,108 @@
+import{A as x,f as o,m as _,J as n,j as t,I as $,k as S}from"./iframe-CfOETbja.js";import{w as T,c as y,i as E,d as D}from"./create-runtime-stories-COP3GmsY.js";import{s as w}from"./style-CNURdi_Z.js";import{E as c}from"./Embed-ME1a_sjz.js";import{H as L}from"./Highlight-DgUtiyfW.js";import{S as r}from"./Section-DrirFjq4.js";import{G as U}from"./Grid-r7-w88qR.js";import{S as A}from"./Select-CJJ3sUBA.js";import"./attributes-q36Eg1F8.js";import"./slot-BP_Qo1ZM.js";import"./_commonjsHelpers-CqkleIqs.js";import"./class-B9SFUanI.js";import"./Container-BbNe0lRi.js";import"./attributes-SGbATqK1.js";import"./Theme-CT5feonC.js";import"./svelte-head-BSK0fUvh.js";import"./html-YMxwMnK0.js";import"./utils-VWytQRnm.js";import"./actions-BF0BegAT.js";import"./Dropdown-Cz1Q1puB.js";import"./each-y_omv-tB.js";import"./Input-BOW3LWXP.js";import"./input--oospc3h.js";const G=`A full page article template with compact header and footer, suitable for scrollytelling and other feature articles.
+
+(Note: The scroller in this example will not preview correctly in the iframe on this page. You can select the example from the menu instead.)
+
+<!-- prettier-ignore -->
+\`\`\`html
+<!-- +page.svelte -->
+<script>
+  import "@onsvisual/svelte-components/css/main.css"; // This is more typically imported in the global +layout.svelte
+  import { Embed, Highlight, Select, Section, Grid } from "@onsvisual/svelte-components";
+<\/script>
+
+<Embed>
+  <Highlight bigText={false} marginBottom={false}>
+    <div style:padding="0 12px">
+      <h2>This is a sub-headline</h2>
+      <p>This is a description of the embedded interactive.</p>
+      <Select label="Select your area" />
+    </div>
+  </Highlight>
+
+  <Section title="Section title" marginTop={true}>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat..
+    </p>
+    <p>
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.
+    </p>
+  </Section>
+
+  <Grid width="medium" caption="This is a caption text">
+    <div class="grid-cell"></div>
+  </Grid>
+
+  <Section title="Another section title">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    </p>
+  </Section>
+
+  <Section title="Yet another section title">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
+  </Section>
+</Embed>
+\`\`\`
+`,H={title:"Templates/Embedded article",component:c,tags:["autodocs"],argTypes:{},parameters:{...T(G),layout:"fullscreen"}},{Story:P}=D();var C=o("<div><h2>This is a sub-headline</h2> <p>This is a description of the embedded interactive.</p> <!></div>"),Y=o(`<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+				laboris nisi ut aliquip ex ea commodo consequat..</p> <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+				pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+				mollit anim id est laborum.</p>`,1),B=o('<div class="grid-cell svelte-1yc884b"></div>'),j=o(`<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+				voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>`),k=o(`<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+				laboris nisi ut aliquip ex ea commodo consequat.</p>`),I=o("<!> <!> <!> <!> <!>",1);function p(h,v){x(v,!1),E(),P(h,{name:"Default",args:{},asChild:!0,children:(g,M)=>{c(g,{children:(f,N)=>{var s=I(),l=_(s);L(l,{bigText:!1,marginBottom:!1,children:(i,a)=>{var e=C();w(e,"",{},{padding:"0 12px"});var q=n(S(e),4);A(q,{label:"Select your area"}),t(i,e)},$$slots:{default:!0}});var u=n(l,2);r(u,{title:"Section title",marginTop:!0,children:(i,a)=>{var e=Y();t(i,e)},$$slots:{default:!0}});var d=n(u,2);U(d,{width:"medium",caption:"This is a caption text",children:(i,a)=>{var e=B();t(i,e)},$$slots:{default:!0}});var m=n(d,2);r(m,{title:"Another section title",children:(i,a)=>{var e=j();t(i,e)},$$slots:{default:!0}});var b=n(m,2);r(b,{title:"Yet another section title",children:(i,a)=>{var e=k();t(i,e)},$$slots:{default:!0}}),t(f,s)},$$slots:{default:!0}})},$$slots:{default:!0},parameters:{__svelteCsf:{rawCode:`<Embed>
+	<Highlight bigText={false} marginBottom={false}>
+		<div style:padding="0 12px">
+			<h2>This is a sub-headline</h2>
+			<p>This is a description of the embedded interactive.</p>
+			<Select label="Select your area" />
+		</div>
+	</Highlight>
+
+	<Section title="Section title" marginTop={true}>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat..
+		</p>
+		<p>
+			Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+			pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+			mollit anim id est laborum.
+		</p>
+	</Section>
+
+	<Grid width="medium" caption="This is a caption text">
+		<div class="grid-cell"></div>
+	</Grid>
+
+	<Section title="Another section title">
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+		</p>
+	</Section>
+
+	<Section title="Yet another section title">
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat.
+		</p>
+	</Section>
+</Embed>`}}}),$()}p.__docgen={data:[],name:"EmbedArticle.stories.svelte"};const J=y(p,H),pe=["Default"],he={...J.Default,tags:["svelte-csf-v5"]};export{he as Default,pe as __namedExportsOrder,H as default};
