4282a2cf18fdb515f1bef6503ede922cd8dc5215
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2026-05-15 17:00:40 +0100

    Updates

---
+++ assets/Footer.stories-CRj7SO32.js
@@ -0,0 +1,11 @@
+import{A as c,f as l,m as f,J as s,j as _,I as d}from"./iframe-Cz6LFa64.js";import{w as v,c as g,i as u,d as h}from"./create-runtime-stories-CgnL4DBC.js";import{F as C}from"./Footer-SrMi3loe.js";import"./attributes-lf33odfc.js";import"./class-B9SFUanI.js";import"./attributes-q36Eg1F8.js";import"./Theme-lq2s_KFk.js";import"./svelte-head-BhIAZEq6.js";import"./html-zt4OZ7wW.js";import"./slot-BP_Qo1ZM.js";import"./style-CNURdi_Z.js";import"./Container-b8LFM69P.js";const D=`A footer component with compact mode and theming options.
+
+<!-- prettier-ignore -->
+\`\`\`html
+<script>
+  import { Footer } from "@onsvisual/svelte-components";
+<\/script>
+
+<Footer />
+\`\`\`
+`,F={title:"Layout/Footer",component:C,tags:["autodocs"],argTypes:{theme:{control:{type:"select"}}},parameters:{...v(D),layout:"fullscreen"}},{Story:t}=h();var k=l("<!> <!> <!>",1);function n(m,i){c(i,!1),u();var o=k(),a=f(o);t(a,{name:"Default",args:{},parameters:{__svelteCsf:{rawCode:"<Footer {...args} />"}}});var r=s(a,2);t(r,{name:"Compact variant",args:{compact:!0},parameters:{__svelteCsf:{rawCode:"<Footer {...args} />"}}});var p=s(r,2);t(p,{name:"Dark theme",args:{theme:"dark"},parameters:{__svelteCsf:{rawCode:"<Footer {...args} />"}}}),_(m,o),d()}n.__docgen={data:[],name:"Footer.stories.svelte"};const e=g(n,F),J=["Default","CompactVariant","DarkTheme"],L={...e.Default,tags:["svelte-csf-v5"]},M={...e.CompactVariant,tags:["svelte-csf-v5"]},O={...e.DarkTheme,tags:["svelte-csf-v5"]};export{M as CompactVariant,O as DarkTheme,L as Default,J as __namedExportsOrder,F as default};
