2d1e47d3b766ba02ede088f68773d0e2952c9b89
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2026-06-08 12:03:53 +0100

    Updates

---
+++ assets/Header.stories-DyGhtt_b.js
@@ -0,0 +1,12 @@
+import{A as _,f as g,m as f,J as t,j as v,I as C}from"./iframe-4GsaYr8F.js";import{w as h,c as u,i as H,d as D}from"./create-runtime-stories-CXyeOlh1.js";import{H as k}from"./Header-BLq3rlpS.js";import"./slot-BP_Qo1ZM.js";import"./attributes-DjyDLnFy.js";import"./class-B9SFUanI.js";import"./attributes-q36Eg1F8.js";import"./Theme-Dnk1f4XZ.js";import"./svelte-head-DkpQ1vxj.js";import"./html-Cdy54qUz.js";import"./style-CNURdi_Z.js";import"./Container-DdX2AgGA.js";import"./SkipLink-B_y1EIDC.js";import"./each-CLhVMnG1.js";import"./this-CpsnL2fn.js";const y=`A header component with compact mode and theming options.
+
+<!-- prettier-ignore -->
+\`\`\`html
+<script>
+  import { Header } from "@onsvisual/svelte-components";
+<\/script>
+
+<Header />
+<!-- page body -->
+\`\`\`
+`,w={title:"Layout/Header",component:k,tags:["autodocs"],argTypes:{theme:{control:{type:"select"}}},parameters:{...h(y),layout:"fullscreen"}},{Story:e}=D();var T=g("<!> <!> <!> <!> <!> <!>",1);function p(i,d){_(d,!1),H();var r=T(),o=f(r);e(o,{name:"Default",args:{},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}});var s=t(o,2);e(s,{name:"Dark mode",args:{theme:"dark"},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}});var m=t(s,2);e(m,{name:"Compact",args:{compact:!0,headerBorder:!0},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}});var c=t(m,2);e(c,{name:"Compact with page title",args:{compact:!0,title:"Page title"},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}});var n=t(c,2);e(n,{name:"Compact dark theme",args:{compact:!0,title:"Page title",theme:"dark"},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}});var l=t(n,2);e(l,{name:"Legacy header",args:{legacy:!0},parameters:{__svelteCsf:{rawCode:"<Header {...args} />"}}}),v(i,r),C()}p.__docgen={data:[],name:"Header.stories.svelte"};const a=u(p,w),R=["Default","DarkMode","Compact","CompactWithPageTitle","CompactDarkTheme","LegacyHeader"],q={...a.Default,tags:["svelte-csf-v5"]},z={...a.DarkMode,tags:["svelte-csf-v5"]},F={...a.Compact,tags:["svelte-csf-v5"]},G={...a.CompactWithPageTitle,tags:["svelte-csf-v5"]},K={...a.CompactDarkTheme,tags:["svelte-csf-v5"]},N={...a.LegacyHeader,tags:["svelte-csf-v5"]};export{F as Compact,K as CompactDarkTheme,G as CompactWithPageTitle,z as DarkMode,q as Default,N as LegacyHeader,R as __namedExportsOrder,w as default};
