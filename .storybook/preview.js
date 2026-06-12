0b8a277a5e26fd7ad4102e03c91e795ba6cc9357
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2026-06-12 09:36:44 +0100

    upgrade to storybook 10

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,6 +9,7 @@ const preview = {
 				date: /Date$/i
 			}
 		},
+
 		options: {
 			storySort: {
 				order: [
@@ -24,6 +25,13 @@ const preview = {
 					"*"
 				]
 			}
+		},
+
+		a11y: {
+			// 'todo' - show a11y violations in the test UI only
+			// 'error' - fail CI on a11y violations
+			// 'off' - skip a11y checks entirely
+			test: "todo"
 		}
 	}
 };
