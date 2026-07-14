26f863c02ea25904cbdf6282c32c0037b0150529
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2026-07-14 16:10:20 +0100

    Updates

---
+++ assets/BackLink.stories-DD4_isnH.js
@@ -0,0 +1,13 @@
+import{ab as n,ad as s}from"./iframe-CETjfACR.js";import{c as a,w as r,i,d as c}from"./create-runtime-stories-BfDsA5km.js";import{B as p}from"./BackLink-2AWZbazb.js";import"./preload-helper-Dp1pzeXC.js";import"./attributes-qW-0xHQB.js";import"./class-BM65IkHf.js";import"./attributes-DayBpKoh.js";const m=`Link that can be used to return to the top of a page, or to a table of contents.
+
+Based on [this ONS Design System component](https://service-manual.ons.gov.uk/design-system/components/back-to-top).
+
+<!-- prettier-ignore -->
+\`\`\`html
+<script>
+  import { BackLink } from "@onsvisual/svelte-components";
+<\/script>
+
+<BackLink href="#top" />
+\`\`\`
+`,f={title:"Navigation/BackLink",component:p,tags:["autodocs"],argTypes:{},parameters:r(m)},{Story:k}=c();function t(e,o){n(o,!1),i(),k(e,{name:"Default",args:{},parameters:{__svelteCsf:{rawCode:"<BackLink {...args} />"}}}),s()}t.__docgen={data:[],name:"BackLink.stories.svelte"};const l=a(t,f),D=["Default"],L={...l.Default,tags:["svelte-csf-v5"]};export{L as Default,D as __namedExportsOrder,f as default};
