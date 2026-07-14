de5cbdd77fe3948c156a4ca7e9b06367811895e9
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2026-07-14 16:09:24 +0100

    fix for col class issue

--- src/lib/components/Grid/GridCell.svelte
+++ src/lib/components/Grid/GridCell.svelte
@@ -51,18 +51,24 @@
 	const width = getContext("width") || "wide";
 	const colWidth = getContext("colWidth") || null;
 	const rowHeight = getContext("rowHeight") || null;
-	const colClass = cls
-		? cls
-		: colWidth
-			? calcSpans(classes?.[width]?.[colWidth] || [], colspan)
-					.map((c) => `ons-col-${c}`)
-					.join(" ")
-			: "";
+
+	const colClass = colWidth
+		? calcSpans(classes?.[width]?.[colWidth] || [], colspan)
+				.map((c) => `ons-col-${c}`)
+				.join(" ")
+		: "";
 </script>
 
 {#if colWidth}
 	<div
-		class="ons-grid__col {colClass}"
+		class="ons-grid__col {cls || colClass}"
+		style:height={typeof rowHeight === "number" ? `${rowHeight}px` : rowHeight}
+	>
+		<slot />
+	</div>
+{:else if cls}
+	<div
+		class="ons-grid__col {cls}"
 		style:height={typeof rowHeight === "number" ? `${rowHeight}px` : rowHeight}
 	>
 		<slot />
